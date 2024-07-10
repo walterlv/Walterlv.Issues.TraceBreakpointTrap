1a1dcffd5f27f4743bb382ddad5214d97d3adf81
walterlv <xblero@gmail.com>
2024-07-10 10:21:21 +0800

    Use simpler demo

--- Walterlv.Issues.TraceBreakpointTrap/Program.cs
+++ Walterlv.Issues.TraceBreakpointTrap/Program.cs
@@ -17,8 +17,9 @@ class Program
 
         AttachDebugger(args);
 
-        var manager = new VolumeManager();
-        manager.Init();
+        // var manager = new VolumeManager();
+        // manager.Init();
+        Tester.Run();
 
         Thread.Sleep(1000);
         Console.WriteLine("Issue may not be reproduced. Exit.");
---
+++ Walterlv.Issues.TraceBreakpointTrap/Tester.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Walterlv.Issues.TraceBreakpointTrap;
+
+public class Tester
+{
+    public static unsafe void Run()
+    {
+        ExecCallbackInNewThread(&Callback);
+    }
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    private static void Callback()
+    {
+    }
+
+    [DllImport("test")]
+    private static unsafe extern void ExecCallbackInNewThread(delegate* unmanaged[Cdecl]<void> callback);
+}
+
+public delegate void test_callback();
